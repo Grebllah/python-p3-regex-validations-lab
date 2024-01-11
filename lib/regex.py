27b2439fd60043b2b152dbe17733c06702bd24d1
James Hallberg <Grebllah81788@gmail.com>
2024-01-11 15:24:04 -0500

    regex validations lab complete

--- lib/regex.py
+++ lib/regex.py
@@ -6,8 +6,8 @@ import re
 name = r"[A-Z][A-z'-]{0,1}[a-z'-]{0,}[\s]{0,1}[A-Z]{0,}[a-z'-]{0,}[A-Z]{0,}[a-z'-]{0,}"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"[(]{0,1}[0-9]{3}[)]{0,1}[\s-]{0,1}[0-9]{3}[-]{0,1}[0-9]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[A-z]{1,}[\w\.]{0,}@[\w]{0,}\.com"
 email_regex = re.compile(email_address)
